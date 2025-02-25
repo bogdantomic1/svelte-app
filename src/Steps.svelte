86827b27878183d19a47a4ac35942389c025698d
Bogdan <60717821+bogdantomic1@users.noreply.github.com>
2025-02-25 15:42:43 +0100

    change

--- src/Steps.svelte
+++ src/Steps.svelte
@@ -122,22 +122,23 @@
             //   });
 
             if (document.getElementById("Email").value.trim() !== "") {
-              fetch(formedUrl, {
-                method: "post",
-                headers: {
-                  "Content-Type": "application/json;charset=UTF-8",
-                },
-                body: payload.data,
-                keepalive: true,
-              })
-                .then(() => {
-                  console.log("submission complete");
-                })
-                .catch((e) => {
-                  console.log(e);
-                  console.log("Form submission failed12312");
-                });
+              // fetch(formedUrl, {
+              //   method: "post",
+              //   headers: {
+              //     "Content-Type": "application/json;charset=UTF-8",
+              //   },
+              //   body: payload.data,
+              //   keepalive: true,
+              // })
+              //   .then(() => {
+              //     console.log("submission complete");
+              //   })
+              //   .catch((e) => {
+              //     console.log(e);
+              //     console.log("Form submission failed12312");
+              //   });
               //track_msdynmkt_testtrigger1_105503091();
+              navigator.sendBeacon(formedUrl, payload.data);
             } else {
               console.log("nema");
               return;
@@ -148,56 +149,57 @@
           true
         );
 
-        // form.addEventListener(
-        //   "submit",
-        //   (e) => {
-        //     e.preventDefault();
-        //     //newSubmit();
+        form.addEventListener(
+          "submit",
+          (e) => {
+            e.preventDefault();
+            //newSubmit();
 
-        //     const serializedForm = d365mktformcapture.serializeForm(
-        //       form,
-        //       mappings
-        //     );
-        //     console.log(JSON.stringify(serializedForm)); // NOTE: enable for debugging //https://cors-anywhere.herokuapp.com
-        //     const payload = serializedForm.SerializedForm.build();
-        //     console.log(payload);
+            const serializedForm = d365mktformcapture.serializeForm(
+              form,
+              mappings
+            );
+            console.log(JSON.stringify(serializedForm)); // NOTE: enable for debugging //https://cors-anywhere.herokuapp.com
+            const payload = serializedForm.SerializedForm.build();
+            console.log(payload);
 
-        //     const captureConfig = {
-        //       FormId: "dee03d17-94e7-ef11-9342-000d3aba33c2",
-        //       FormApiUrl:
-        //         "https://cors-anywhere.herokuapp.com/https://public-eur.mkt.dynamics.com/api/v1.0/orgs/9bc5e4fe-4bda-ef11-b8e4-000d3ab73d5f/landingpageforms",
-        //     };
-        //     const formedUrl =
-        //       "https://public-eur.mkt.dynamics.com/api/v1.0/orgs/9bc5e4fe-4bda-ef11-b8e4-000d3ab73d5f/landingpageforms/forms/dee03d17-94e7-ef11-9342-000d3aba33c2";
+            const captureConfig = {
+              FormId: "dee03d17-94e7-ef11-9342-000d3aba33c2",
+              FormApiUrl:
+                "https://cors-anywhere.herokuapp.com/https://public-eur.mkt.dynamics.com/api/v1.0/orgs/9bc5e4fe-4bda-ef11-b8e4-000d3ab73d5f/landingpageforms",
+            };
+            const formedUrl =
+              "https://public-eur.mkt.dynamics.com/api/v1.0/orgs/9bc5e4fe-4bda-ef11-b8e4-000d3ab73d5f/landingpageforms/forms/dee03d17-94e7-ef11-9342-000d3aba33c2";
 
-        //     // d365mktformcapture
-        //     //   .submitForm(captureConfig, payload)
-        //     //   .catch((e) => {
-        //     //     console.log(e);
-        //     //     console.log('Form submission failed');
-        //     //   });
+            // d365mktformcapture
+            //   .submitForm(captureConfig, payload)
+            //   .catch((e) => {
+            //     console.log(e);
+            //     console.log('Form submission failed');
+            //   });
 
-        //     if (document.getElementById("Email").value.trim() !== "") {
-        //       fetch(formedUrl, {
-        //         method: "post",
-        //         headers: {
-        //           "Content-Type": "application/json;charset=UTF-8",
-        //         },
-        //         body: payload.data,
-        //         keepalive: true,
-        //       }).then(() => {
-        //         console.log("submission complete");
-        //       });
-        //       track_msdynmkt_testtrigger1_105503091();
-        //     } else {
-        //       console.log("nema");
-        //       return;
-        //     }
+            if (document.getElementById("Email").value.trim() !== "") {
+              // fetch(formedUrl, {
+              //   method: "post",
+              //   headers: {
+              //     "Content-Type": "application/json;charset=UTF-8",
+              //   },
+              //   body: payload.data,
+              //   keepalive: true,
+              // }).then(() => {
+              //   console.log("submission complete");
+              // });
+              // track_msdynmkt_testtrigger1_105503091();
+              navigator.sendBeacon(formedUrl, JSON.stringify(payload.data));
+            } else {
+              console.log("nema");
+              return;
+            }
 
-        //     console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        //   },
-        //   true
-        // );
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+          },
+          true
+        );
       });
 
     console.log("na dnu mscrmmkt func");
