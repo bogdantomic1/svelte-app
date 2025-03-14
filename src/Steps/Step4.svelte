a4c427d26db382c232b8a026bf8693e64a9cdf90
Bogdan <60717821+bogdantomic1@users.noreply.github.com>
2025-03-14 11:07:37 +0100

    change

--- src/Steps/Step4.svelte
+++ src/Steps/Step4.svelte
@@ -20,26 +20,26 @@
   import { onMount } from "svelte";
   import { createEventDispatcher } from "svelte";
 
-  function track_msdynmkt_exittrigger_135927779() {
-    window["msdynmkt"].setUser({ authId: emailField.value }); // ID, e-mail or phone number - see instructions
-    window["msdynmkt"].trackEvent({
-      name: "msdynmkt_exittrigger_135927779", //Trigger title: Exit trigger
-      ingestionKey:
-        "b1d765b9c293466bb1ef2ac825fc18f8-3d35c0db-60a3-4e5b-a38a-626cf4a27efb-7558",
-      version: "1.0.0",
-      properties: {
-        bindingid: emailField.value,
-      },
-    });
-
-    console.log("proslo");
-  }
-
   onMount(() => {
     console.log("Component is fully loaded!");
 
     // Example: Get form values
     const emailField = document.getElementById("Email");
+
+    function track_msdynmkt_exittrigger_135927779() {
+      window["msdynmkt"].setUser({ authId: emailField.value }); // ID, e-mail or phone number - see instructions
+      window["msdynmkt"].trackEvent({
+        name: "msdynmkt_exittrigger_135927779", //Trigger title: Exit trigger
+        ingestionKey:
+          "b1d765b9c293466bb1ef2ac825fc18f8-3d35c0db-60a3-4e5b-a38a-626cf4a27efb-7558",
+        version: "1.0.0",
+        properties: {
+          bindingid: emailField.value,
+        },
+      });
+
+      console.log("proslo");
+    }
     if (emailField.value) {
       console.log("Email:", emailField.value);
       track_msdynmkt_exittrigger_135927779();
