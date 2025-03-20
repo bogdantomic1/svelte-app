208ba2233f80eef1a8a53a7ec93b6ae388fc8605
Bogdan <60717821+bogdantomic1@users.noreply.github.com>
2025-03-20 15:11:09 +0100

    change

--- src/Steps/Step2.svelte
+++ src/Steps/Step2.svelte
@@ -43,7 +43,7 @@
           <option value="3">Undefined</option>
         </select>
       </div>
-      <button on:click={() => {setStep(3); removePageHideListener();} } class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Next Step</button>
+      <button on:click={() => {removePageHideListener();} } class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Next Step</button>
     </div>
   </div>
 </section>
