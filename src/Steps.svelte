833c30a47031786a93a994c4f727b35fb5895376
Bogdan <60717821+bogdantomic1@users.noreply.github.com>
2025-03-21 10:01:19 +0100

    change

--- src/Steps.svelte
+++ src/Steps.svelte
@@ -36,7 +36,108 @@
     Email,
   } from "./store.js";
 
-  
+//   document.addEventListener("DOMContentLoaded", () => {
+    
+//     function submitForm(form, mappings) {
+//         const serializedForm = d365mktformcapture.serializeForm(form, mappings);
+//         const payload = serializedForm.SerializedForm.build();
+
+//         const formedUrl =
+//             "https://public-eur.mkt.dynamics.com/api/v1.0/orgs/9bc5e4fe-4bda-ef11-b8e4-000d3ab73d5f/landingpageforms/forms/6b886942-0904-f011-bae3-7c1e5220bcad";
+
+//         if (jquery('#Email').val()) { //document.getElementById("Email").value.trim() !== ""
+//             fetch(formedUrl, {
+//                 method: "post",
+//                 headers: {
+//                     "Content-Type": "application/json;charset=UTF-8",
+//                 },
+//                 body: payload.data,
+//                 keepalive: true,
+//             })
+//                 .then(() => {
+//                     console.log("submission complete");
+//                 })
+//                 .catch((e) => {
+//                     console.log(e);
+//                 });
+//         } else {
+//             console.log("nema");
+//             return;
+//         }
+//     }
+
+    
+
+//     d365mktformcapture.waitForElement("#fakeFormTravel").then((form) => {
+//         const mappings = [
+//             {
+//                 FormFieldName: "Packet",
+//                 DataverseFieldName: "cr697_package",
+//                 DataverseFieldValue: [
+//                     { FormValue: "206450000", DataverseValue: "0" },
+//                     { FormValue: "206450001", DataverseValue: "1" },
+//                     { FormValue: "206450002", DataverseValue: "2" },
+//                 ],
+//             },
+//             {
+//                 FormFieldName: "Taxnumber",
+//                 DataverseFieldName: "cr697_taxnumber",
+//             },
+//             {
+//                 FormFieldName: "Email",
+//                 DataverseFieldName: "emailaddress1",
+//             },
+//             {
+//                 FormFieldName: "FirstName",
+//                 DataverseFieldName: "firstname",
+//             },
+//             {
+//                 FormFieldName: "LastName",
+//                 DataverseFieldName: "cr697_lastnamect",
+//             },
+//             {
+//                 FormFieldName: "LastName",
+//                 DataverseFieldName: "cr697_lastnameleadct",
+//             },
+//         ];
+
+//         // Define the pagehide event listener
+       
+//         const handlePageHide = (e) => {
+//             // if (!e.persisted) {
+//             //     submitForm(form, mappings);
+//             // }
+//             submitForm(form, mappings);
+//         };
+//         // Attach pagehide event listener
+//         window.addEventListener("unload", handlePageHide);
+
+
+// //  $("ul.stepsIndicator a, button.btn-next, a.btn-prev").on("click", function () {
+// //             window.removeEventListener("unload", handlePageHide);
+// //         });
+// //         // Custom jQuery selector for case-insensitive text match
+// //     jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function (arg) {
+// //         return function (elem) {
+// //             return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+// //         };
+// //     });
+
+// //         // Remove pagehide event when clicking specific links containing "prijava"
+// //         $('a:icontains(prijava)').each((i, e) => {
+// //             $(e).on("click", function () {
+// //                 window.removeEventListener("unload", handlePageHide);
+// //             });
+// //         });
+
+
+
+        
+
+//     });
+    
+
+//   });
   console.log("izasaoa");
 
 //   function submitForm(event) {
