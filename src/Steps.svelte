687e9d4e452fb28b410b9fc2c80a72c8960aafdf
Bogdan <60717821+bogdantomic1@users.noreply.github.com>
2025-04-24 11:50:48 +0200

    change

--- src/Steps.svelte
+++ src/Steps.svelte
@@ -50,25 +50,25 @@ function submitForm(form, mappings) {
 
  if (document.getElementById("Email")?.value.trim() !== "") { //document.getElementById("Email").value.trim() !== ""
  console.log("fetching");   
-//  fetch(formedUrl, {
-//         method: "post",
-//         headers: {
-//                     "Content-Type": "application/json;charset=UTF-8",
-//                 },
-//         body: payload.data,
-//         keepalive: true,
-//     })
-//         .then(() => {
-//             console.log("submission complete");
-//         })
-//         .catch((e) => {
-//             console.log(e);
-//         });
+ fetch(formedUrl, {
+        method: "post",
+        headers: {
+                    "Content-Type": "application/json;charset=UTF-8",
+                },
+        body: payload,
+        keepalive: true,
+    })
+        .then(() => {
+            console.log("submission complete");
+        })
+        .catch((e) => {
+            console.log(e);
+        });
           //const data = new FormData(document.getElementById('fakeFormTravel'))
-           const blob = new Blob([payload.data], {
-             type: 'application/json;charset=UTF-8',
-           });
-          navigator.sendBeacon(formedUrl, blob);
+          //  const blob = new Blob([payload.data], {
+          //    type: 'application/json;charset=UTF-8',
+          //  });
+          // navigator.sendBeacon(formedUrl, blob);
         
   } 
   else {console.log("nema");return;}
