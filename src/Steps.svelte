8c4aef3b92da3974729e2e362e08aaa520604b57
Bogdan <60717821+bogdantomic1@users.noreply.github.com>
2025-05-09 15:16:19 +0200

    change

--- src/Steps.svelte
+++ src/Steps.svelte
@@ -1,17 +1,17 @@
 <script>
-  import Step1 from "./Steps/Step1.svelte";
-  import Step2 from "./Steps/Step2.svelte";
-  import Step3 from "./Steps/Step3.svelte";
-  import Step4 from "./Steps/Step4.svelte";
-  import AccidentStep1 from "./AccidentSteps/Step1.svelte";
-  import AccidentStep2 from "./AccidentSteps/Step2.svelte";
-  import AccidentStep3 from "./AccidentSteps/Step3.svelte";
-  import AccidentStep4 from "./AccidentSteps/Step4.svelte";
-  import FakeForm from "./FakeForm.svelte";
-  import FakeFormAccident from "./FakeFormAccident.svelte";
-  import FakeFormTravel from "./FakeFormTravel.svelte";
-  import FakeFormTravelComplete from "./FakeFormTravelComplete.svelte";
-  import  jquery from "jquery";
+  import Step1 from './Steps/Step1.svelte'
+  import Step2 from './Steps/Step2.svelte'
+  import Step3 from './Steps/Step3.svelte'
+  import Step4 from './Steps/Step4.svelte'
+  import AccidentStep1 from './AccidentSteps/Step1.svelte'
+  import AccidentStep2 from './AccidentSteps/Step2.svelte'
+  import AccidentStep3 from './AccidentSteps/Step3.svelte'
+  import AccidentStep4 from './AccidentSteps/Step4.svelte'
+  import FakeForm from './FakeForm.svelte'
+  import FakeFormAccident from './FakeFormAccident.svelte'
+  import FakeFormTravel from './FakeFormTravel.svelte'
+  import FakeFormTravelComplete from './FakeFormTravelComplete.svelte'
+  import jquery from 'jquery'
 
   // import jQuery from 'jquery'
 
@@ -34,200 +34,184 @@
     DateOfbirth,
     Startingdayofinsurance,
     Email,
-  } from "./store.js";
+  } from './store.js'
 
-
-  
-  document.addEventListener("DOMContentLoaded", () => {
-
-console.log("USAOOOOAOOAOAOAOOAO steps");
-
-function submitForm(form, mappings) {
- const serializedForm = d365mktformcapture.serializeForm(form, mappings);
- const payload = serializedForm.SerializedForm.build();
-
- const formedUrl = "https://public-eur.mkt.dynamics.com/api/v1.0/orgs/031746fd-6eb4-43a8-9fa4-4488bcfad3ba/landingpageforms/forms/8fb7be5e-5120-f011-9989-000d3aa92e0a"
- //const formedUrl = "https://webhook.site/0d2677a7-9573-4972-bf4f-bc220a59bc5c";
- if (document.getElementById("Email")?.value.trim() !== "") { //document.getElementById("Email").value.trim() !== ""
- console.log("fetching");
- fetch(formedUrl, {
-        method: "post",
-        headers: {
-                    "Content-Type": "application/json",
-                },
-        body: payload.data,
-        keepalive: true,
-    })
-        .then(() => {
-            console.log("submission complete");
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('USAOOOOAOOAOAOAOOAO steps')
+    function submitForm(form, mappings) {
+      const serializedForm = d365mktformcapture.serializeForm(form, mappings)
+      const payload = serializedForm.SerializedForm.build()
+      const formedUrl =
+        'https://public-eur.mkt.dynamics.com/api/v1.0/orgs/031746fd-6eb4-43a8-9fa4-4488bcfad3ba/landingpageforms/forms/8fb7be5e-5120-f011-9989-000d3aa92e0a'
+      //const formedUrl = "https://webhook.site/0d2677a7-9573-4972-bf4f-bc220a59bc5c";
+      if (document.getElementById('Email')?.value.trim() !== '') {
+        //document.getElementById("Email").value.trim() !== ""
+        console.log('fetching')
+        fetch(formedUrl, {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: payload.data,
+          keepalive: true,
         })
-        .catch((e) => {
-            console.log(e);
-        });
-         
-        
-          // const blob = new Blob([JSON.stringify(payload.data)], {
-          //     type: 'application/json'
-          // });
+          .then(() => {
+            console.log('submission complete')
+          })
+          .catch((e) => {
+            console.log(e)
+          })
 
-          // const success = navigator.sendBeacon(formedUrl, blob);
+        // const blob = new Blob([JSON.stringify(payload.data)], {
+        //     type: 'application/json'
+        // });
+        // const success = navigator.sendBeacon(formedUrl, blob);
+        // if (success) {
+        //   console.log("✅ sendBeacon request queued successfully");
+        // } else {
+        //   console.error("❌ sendBeacon failed to queue");
+        // }
+      } else {
+        console.log('nema')
+        return
+      }
+    }
+    d365mktformcapture.waitForElement('#fakeFormTravel').then((form) => {
+      const mappings = [
+        // {
+        //     FormFieldName: "Packet",
+        //     DataverseFieldName: "cr697_package",
+        //     DataverseFieldValue: [
+        //         { FormValue: "206450000", DataverseValue: "0" },
+        //         { FormValue: "206450001", DataverseValue: "1" },
+        //         { FormValue: "206450002", DataverseValue: "2" },
+        //     ],
+        // },
+        // {
+        //     FormFieldName: "Taxnumber",
+        //     DataverseFieldName: "cr697_taxnumber",
+        // },
+        {
+          FormFieldName: 'Email',
+          DataverseFieldName: 'emailaddress1',
+        },
+        {
+          FormFieldName: 'FirstName',
+          DataverseFieldName: 'firstname',
+        },
+        {
+          FormFieldName: 'LastName',
+          DataverseFieldName: 'lastname',
+        },
+        {
+          FormFieldName: 'Email',
+          DataverseFieldName: 'ct_emailct',
+        },
+        {
+          FormFieldName: 'FirstName',
+          DataverseFieldName: 'ct_firstnamect',
+        },
+        {
+          FormFieldName: 'LastName',
+          DataverseFieldName: 'ct_lastnamect',
+        },
+        {
+          FormFieldName: 'PhoneNumber',
+          DataverseFieldName: 'in2_input_mobile_phone',
+        },
+        // {
+        //     FormFieldName: "FirstName",
+        //     DataverseFieldName: "ct_firstnamect",
+        // },
+        // {
+        //     FormFieldName: "LastName",
+        //     DataverseFieldName: "cr697_lastnameleadct",
+        // },
+      ]
 
-          // if (success) {
-          //   console.log("✅ sendBeacon request queued successfully");
-          // } else {
-          //   console.error("❌ sendBeacon failed to queue");
-          // }
-  } 
-  else {console.log("nema");return;}
-}
+      // Define the pagehide event listener
 
+      const handlePageHide = (e) => {
+        // if (!e.persisted) {
+        //     submitForm(form, mappings);
+        // }
+        console.log('pagehide event triggered')
+        submitForm(form, mappings)
+        // if (!e.persisted) {
+        //   submitForm(form, mappings)
+        // }
+      }
 
+      // Attach pagehide event listener
 
-d365mktformcapture.waitForElement("#fakeFormTravel").then((form) => {
-const mappings = [
-    // {
-    //     FormFieldName: "Packet",
-    //     DataverseFieldName: "cr697_package",
-    //     DataverseFieldValue: [
-    //         { FormValue: "206450000", DataverseValue: "0" },
-    //         { FormValue: "206450001", DataverseValue: "1" },
-    //         { FormValue: "206450002", DataverseValue: "2" },
-    //     ],
-    // },
-    // {
-    //     FormFieldName: "Taxnumber",
-    //     DataverseFieldName: "cr697_taxnumber",
-    // },
-    {
-        FormFieldName: "Email",
-        DataverseFieldName: "emailaddress1",
-    },
-    {
-        FormFieldName: "FirstName",
-        DataverseFieldName: "firstname",
-    },
-    {
-        FormFieldName: "LastName",
-        DataverseFieldName: "lastname",
-    },
-    {
-        FormFieldName: "Email",
-        DataverseFieldName: "ct_emailct",
-    },
-    {
-        FormFieldName: "FirstName",
-        DataverseFieldName: "ct_firstnamect",
-    },
-    {
-        FormFieldName: "LastName",
-        DataverseFieldName: "ct_lastnamect",
-    },
-    {
-        FormFieldName: "PhoneNumber",
-        DataverseFieldName: "in2_input_mobile_phone",
-    },
-    // {
-    //     FormFieldName: "FirstName",
-    //     DataverseFieldName: "ct_firstnamect",
-    // },
-    // {
-    //     FormFieldName: "LastName",
-    //     DataverseFieldName: "cr697_lastnameleadct",
-    // },
-];
+      window.addEventListener('unload', handlePageHide) //ovdeeeee unload
+      form.addEventListener('submit', handlePageHide)
+      //  $("ul.stepsIndicator a, button.btn-next, a.btn-prev").on("click", function () {
+      //             window.removeEventListener("unload", handlePageHide);
+      //         });
+      //         // Custom jQuery selector for case-insensitive text match
+      //     jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function (arg) {
+      //         return function (elem) {
+      //             return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+      //         };
+      //     });
 
-// Define the pagehide event listener
+      //         // Remove pagehide event when clicking specific links containing "prijava"
+      //         $('a:icontains(prijava)').each((i, e) => {
+      //             $(e).on("click", function () {
+      //                 window.removeEventListener("unload", handlePageHide);
+      //             });
+      //         });
 
-const handlePageHide = (e) => {
-    // if (!e.persisted) {
-    //     submitForm(form, mappings);
-    // }
-    console.log("pagehide event triggered");``
-    //submitForm(form, mappings);
-    if (!e.persisted) {
-            submitForm(form, mappings);
-        }
-};
-// Attach pagehide event listener
- window.addEventListener("pagehide", handlePageHide); ///ovdeeeee unload
- form.addEventListener("submit", handlePageHide);
-//  $("ul.stepsIndicator a, button.btn-next, a.btn-prev").on("click", function () {
-//             window.removeEventListener("unload", handlePageHide);
-//         });
-//         // Custom jQuery selector for case-insensitive text match
-//     jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function (arg) {
-//         return function (elem) {
-//             return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-//         };
-//     });
+      document.querySelectorAll('a').forEach((element) => {
+        element.addEventListener('click', () => {
+          console.log('skinuo event')
+          window.removeEventListener('unload', handlePageHide)
+        })
+      })
+    })
+  })
 
-//         // Remove pagehide event when clicking specific links containing "prijava"
-//         $('a:icontains(prijava)').each((i, e) => {
-//             $(e).on("click", function () {
-//                 window.removeEventListener("unload", handlePageHide);
-//             });
-//         });
+  function formSubmition(event) {
+    event.preventDefault()
+    console.log('form submited1')
+    let form = document.getElementById('fakeFormTravel')
+    if (form) {
+      let event = new Event('submit', { bubbles: true, cancelable: true })
+      form.dispatchEvent(event)
+      console.log('Submit event dispatched!')
+    } else {
+      console.error('Form not found!')
+    }
 
-document.querySelectorAll("a").forEach((element) => {
-element.addEventListener("click", () => {
-console.log("skinuo event");
-window.removeEventListener("unload", handlePageHide);
-});
-});
+    console.log('form ended')
+  }
 
-
-
-});
-
-
-});
-
-
-
-function formSubmition(event) {
-  event.preventDefault();
-  console.log("form submited1");
-  let form = document.getElementById("fakeFormTravel");
-  if (form) {
-         let event = new Event("submit", { bubbles: true, cancelable: true });
-         form.dispatchEvent(event);
-         console.log("Submit event dispatched!");
-     } else {
-         console.error("Form not found!");
-     }
-
- 
-
-  console.log("form ended");
-}
-
-  let count_value;
+  let count_value
 
   currentStep.subscribe((value) => {
-    count_value = value;
-  });
+    count_value = value
+  })
 
-  let beforeLoadSet = false;
+  let beforeLoadSet = false
 
   let setStep = (val) => {
-    console.log(val);
-    currentStep.update((n) => val);
+    console.log(val)
+    currentStep.update((n) => val)
 
     if ($currentStep == 4 && !beforeLoadSet) {
-      console.log("before unload set");
-      beforeLoadSet = true;
+      console.log('before unload set')
+      beforeLoadSet = true
       //window.addEventListener('unload', event => submitForm(event));
       //window.addEventListener("beforeunload", (event) => {
-        //event.preventDefault();
-        //submitForm();
+      //event.preventDefault();
+      //submitForm();
       //});
     }
-  };
+  }
 
- 
-
-  let active = "bg-gray-100 border-red-500 text-red-500 rounded-t";
-  let notactive = "border-gray-200 hover:text-gray-900";
+  let active = 'bg-gray-100 border-red-500 text-red-500 rounded-t'
+  let notactive = 'border-gray-200 hover:text-gray-900'
 </script>
 
 <section class="text-gray-600 body-font">
@@ -279,7 +263,7 @@ function formSubmition(event) {
         </svg>STEP 1
       </a>
       <a
-        on:click={() =>  setStep(2)}
+        on:click={() => setStep(2)}
         class="{$currentStep == 2
           ? active
           : notactive} sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
@@ -297,7 +281,9 @@ function formSubmition(event) {
         </svg>STEP 2
       </a>
       <a
-        on:click={() => {setStep(3); }}
+        on:click={() => {
+          setStep(3)
+        }}
         class="{$currentStep == 3
           ? active
           : notactive} sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider"
@@ -337,25 +323,25 @@ function formSubmition(event) {
     </div>
 
     {#if count_value == 1}
-      {#if $formType == "Travel"}
+      {#if $formType == 'Travel'}
         <Step1 />
       {:else}
         <AccidentStep1 />
       {/if}
     {:else if count_value == 2}
-      {#if $formType == "Travel"}
+      {#if $formType == 'Travel'}
         <Step2 />
       {:else}
         <AccidentStep2 />
       {/if}
     {:else if count_value == 3}
-      {#if $formType == "Travel"}
+      {#if $formType == 'Travel'}
         <Step3 />
       {:else}
         <AccidentStep3 />
       {/if}
     {:else if count_value == 4}
-      {#if $formType == "Travel"}
+      {#if $formType == 'Travel'}
         <Step4 on:userSubmit={formSubmition} />
       {:else}
         <AccidentStep4 on:userSubmit={removeListener} />
